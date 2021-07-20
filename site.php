a7f35b214877f21a1ef44e618f667eac0b47183b
Jesse <jessedaniel@live.ca>
2021-07-20 12:47:24 -0600

    use form for add program

--- site.php
+++ site.php
@@ -8,10 +8,12 @@
 </head>
 <body>
   <form action="site.php" method="get">
-    Name: <input type="text" name="name">
-    <input type="submit">
+    <input type="number" name="num1">
     <br>
-   Your name is <?php  echo $_GET["name"]?>
+    <input type="number" name="num2">
+    <input type="submit">
 </form>
+
+Answer: <?php echo $_GET["num1"] + $_GET["num2"]?>
 </body>
 </html>
