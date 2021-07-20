3550fde3949322cd079553d02a37a9ec0e13a442
Jesse <jessedaniel@live.ca>
2021-07-20 13:16:22 -0600

    use post

--- site.php
+++ site.php
@@ -7,14 +7,14 @@
   <title>Document</title>
 </head>
 <body>
-  <form action="site.php" method="get">
+  <form action="site.php" method="post">
    Password: <input type="password" name="password"> <br>
     <input type="submit">
 </form>
 <br><br>
 
 <?php 
-echo $_GET["password"];
+echo $_POST["password"];
 ?>
 </body>
 </html>
