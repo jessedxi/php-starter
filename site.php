514ff8c276c09bc628c4bb7fd9d3fe6dba5e9136
Jesse <jessedaniel@live.ca>
2021-07-20 11:07:51 -0600

    use variable reassignment

--- site.php
+++ site.php
@@ -8,10 +8,11 @@
 </head>
 <body>
   <?php 
-  $characterName = "John";
+  $characterName = "Tom";
   $characterAge = 35;
   echo "There once was a man named $characterName <br>";
   echo "He was $characterAge years old <br>";
+  $characterName = "Mike";
   echo "He really liked the name $characterName <br>";
   echo "But didn't like being $characterAge <br>";
   ?>
