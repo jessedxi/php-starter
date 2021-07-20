1f127c301f46428f0304a504c3a5df51cfc359d1
Jesse <jessedaniel@live.ca>
2021-07-20 13:37:08 -0600

    associative arrays

--- site.php
+++ site.php
@@ -9,15 +9,11 @@
 <body>
 
 <form action="site.php" method="post">
-Apples: <input type="checkbox" name="fruits[]" value="apples" ><br>
-Oranges: <input type="checkbox" name="fruits[]" value="oranges" ><br>
-Pears: <input type="checkbox" name="fruits[]" value="pears" ><br>
 <input type="submit">
 <br><br>
   
   <?php
-  $fruits = $_POST["fruits"];
-  echo $fruits[1];
+
   ?>
 </body>
 </html>
