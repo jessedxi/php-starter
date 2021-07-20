15ac1460137d103f32214f92c382b47d887b1da5
Jesse <jessedaniel@live.ca>
2021-07-20 13:33:02 -0600

    set up check boxes

--- site.php
+++ site.php
@@ -9,7 +9,9 @@
 <body>
 
 <form action="site.php" method="post">
-
+Apples: <input type="checkbox" name="fruits[]" value="apples" ><br>
+Oranges: <input type="checkbox" name="fruits[]" value="oranges" ><br>
+Pears: <input type="checkbox" name="fruits[]" value="pears" ><br>
 <input type="submity">
   <?php
  
