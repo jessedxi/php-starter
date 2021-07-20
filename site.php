1e363a0311aed121bd60551db611f90771420f54
Jesse <jessedaniel@live.ca>
2021-07-20 13:47:05 -0600

    functions

--- site.php
+++ site.php
@@ -7,16 +7,9 @@
   <title>Document</title>
 </head>
 <body>
-
-<form action="site.php" method="post">
-  <input type="text" name="student">
-<input type="submit">
-</form>
-<br><br>
   
   <?php
-  $grades = array("Jim" =>"A+", "Pam"=>"B-", "Oscar"=>"C+");
-  echo $grades[$_POST["student"]];
+  
   ?>
 </body>
 </html>
