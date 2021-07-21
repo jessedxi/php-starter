b56ffcdc3d5f0184040070465f9b534df3690621
Jesse <jessedaniel@live.ca>
2021-07-21 11:26:39 -0600

    while loop setup

--- site.php
+++ site.php
@@ -8,34 +8,10 @@
 </head>
 <body>
 
-<form action="site.php" method ="post">
-What was your grade? <br>
- <input type="text" name="grade" > <br>
-<input type="submit">
-</form>
+
 
   <?php
-$grade = $_POST["grade"];
-switch ($grade) {
-    case "A":
-        echo "You did amazing!";
-        break;
-    case "B":
-        echo "You did pretty good!";
-        break;
-    case "C":
-        echo "You did poorly";
-        break;
-    case "D":
-        echo "You did badly";
-        break;
-    case "F":
-        echo "You failed!";
-        break;
-    default:
-        echo "Invalid Grade";
-        break;
-}
+
 ?>
 </body>
 </html>
