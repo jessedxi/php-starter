a3043bdd42fdc9376df8db082656fb97d24da904
Jesse <jessedaniel@live.ca>
2021-07-21 11:51:31 -0600

    loop through array

--- site.php
+++ site.php
@@ -11,14 +11,9 @@
 
 
 <?php
-$index = 1;
-while ($index <= 5) {
-    echo "$index <br>";
-    $index++;
-}
-
-for ($i = 1; $i <= 5; $i++) {
-    echo "$i <br>";
+$luckyNumbers = array(4, 8, 14, 16, 23, 42);
+for ($i = 0; $i <= 5; $i++) {
+    echo "$luckyNumbers[$i] <br>";
 }
 ?>
 </body>
