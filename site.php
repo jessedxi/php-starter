7c4866ca07a21dd13ba95eab837cfc415c6a30a2
Jesse <jessedaniel@live.ca>
2021-07-21 11:23:20 -0600

    complete switch statement

--- site.php
+++ site.php
@@ -9,14 +9,31 @@
 <body>
 
 <form action="site.php" method ="post">
-What was your grade?
- <input type="text" name="grade" > 
+What was your grade? <br>
+ <input type="text" name="grade" > <br>
 <input type="submit">
 </form>
-  
+
   <?php
-  $grade = $_POST["grade"];
-  echo $grade;
-  ?>
+$grade = $_POST["grade"];
+switch ($grade) {
+    case "A":
+        echo "You did amazing!";
+        break;
+    case "B":
+        echo "You did pretty good!";
+        break;
+    case "C":
+        echo "You did poorly";
+        break;
+    case "D":
+        echo "You did badly";
+        break;
+    case "F":
+        echo "You failed!";
+        break;
+
+}
+?>
 </body>
 </html>
