8b1234b7abc5ad6c5d9c653c98bed2101ea5a81a
Jesse <jessedaniel@live.ca>
2021-07-21 11:07:07 -0600

    setup switch statements

--- site.php
+++ site.php
@@ -9,29 +9,14 @@
 <body>
 
 <form action="site.php" method ="post">
-First Num:<input type="number" step="0.1" name="num1"> <br>
-OP: <input type="text" name="op" > <br>
-Second Num:<input type="number" name="num2"> <br>
+What was your grade?
+ <input type="text" name="grade" > 
 <input type="submit">
 </form>
   
   <?php
-  $num1 = $_POST["num1"];
-  $num2 = $_POST["num2"];
-  $op = $_POST["op"];
-
-  if( $op == "+") {
-    echo $num1 + $num2;
-  } elseif ($op == "-") {
-    echo $num1 - $num2;
-  } elseif ($op == "/") {
-    echo $num1 / $num2;
-  } elseif ($op == "*") {
-    echo $num1 * $num2;
-  } else {
-    echo "Invalid Operator";
-  }
-  
+  $grade = $_POST["grade"];
+  echo $grade;
   ?>
 </body>
 </html>
